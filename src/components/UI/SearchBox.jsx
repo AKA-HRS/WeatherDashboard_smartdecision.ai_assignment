4ec48b49ac850cfa25c5aa21fc7b8f3f4ef25021
AKA-HRS <harshraj2003sinha@gmail.com>
2024-07-18 11:56:56 +0530

    added keyup feature

--- src/components/UI/SearchBox.jsx
+++ src/components/UI/SearchBox.jsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Search } from "@mui/icons-material";
 import { Box, IconButton, TextField, Tooltip } from "@mui/material";
-import { data } from "../../api/weather"; // Importing the data fetching function
 import { WeatherData } from "../../context/WeatherCardDataProvider"; // Importing the WeatherData context
 
 function SearchBox() {
   const [search, setSearch] = useState("Delhi"); // Initializing the search state with default value "Delhi"
-  const { handelSearch } = useContext(WeatherData); 
+  const { handelSearch } = useContext(WeatherData);
   return (
     <Box width="100%">
       <TextField
@@ -15,13 +14,17 @@ function SearchBox() {
         variant="outlined"
         value={search} // Bind the search input value to the state
         onChange={(e) => setSearch(e.target.value)} // Update the search state on input change
+        onKeyDown={(e) => (e.key === "Enter" ? handelSearch(search) : "")} // Call handleSearch on Enter key press
         sx={{
           width: "100%", // Set the width of the text field to 100%
         }}
         InputProps={{
           endAdornment: (
             <Tooltip title="Search" color="secondary">
-              <IconButton color="primary" onClick={()=>handelSearch(search)}>
+              <IconButton
+                color="primary"
+                onClick={() => handelSearch(search)} // Call handleSearch on click
+              >
                 <Search />
                 {/* Search icon button to trigger the search operation */}
               </IconButton>
